d5f772077da56f8a44f97ff249c1339183b8f0be
Shahboz Razzoqov <69413364+Shahbozjon@users.noreply.github.com>
2021-07-15 11:17:46 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// let pic = document.querySelector('.work-1');
+
+// let text = document.createTextNode("GO TO WEBSITE");
+
+// pic.addEventListener('mouseover', function() {
+//   pic.appendChild(text);
+// });
+// pic.addEventListener('mouseleave', function() {
+//   pic.appendChild();
+// });
